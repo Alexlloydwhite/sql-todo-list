de1b58daf28220e286448157e6a2c35977886cae
Alex White <alexlloydwhite@gmail.com>
2021-04-23 15:05:06 -0500

    setting up the server..

---
+++ server/server.js
@@ -0,0 +1,25 @@
+// source in express
+const express = require('express');
+// source in bodyParser
+const bodyParser = require('body-parser');
+// source in router
+const listRouter = require('./modules/list.router');
+
+// declare app
+const app = express();
+
+// helps to ensure that inputs do not come back as undefined
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// /todo -> correct router
+app.use('/todo', listRouter);
+
+// server back static files by default
+app.use(express.static('server/public'));
+
+// start listening for requests on a specific PORT
+// setting up as env because i know i will eventually deploy to heroku..
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+});
