30fc131af5f0c636b94c3569843a7ef52da3341a
Alex White <alexlloydwhite@gmail.com>
2021-04-24 11:50:22 -0500

    +new task adds form to list

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,13 +3,26 @@ $(document).ready(function () {
     console.log('JQ');
     // on page load, displays db
     getList();
-    // click listener to add new action item
-    $('#submitBtn').on('click', addNewItem);
     // click listener to delete item from db
     $('.outPut').on('click', '#delete-item', deleteHandler);
     // click listener to mark action item as complete TODO - Toggle!!
     $('.outPut').on('click', '.listItem', markAsComplete);
+    //click listener to trigger from to add new task!
+    $('#newTaskBtn').on('click', addNewTask);
+    // click listener for submit new item 
+    $('.outPut').on('click', '#submitBtn', addNewItem);
 })
+
+function addNewTask() {
+    $('.outPut').append(`
+        <li class="inputForm"> 
+            <input type="text" id="listItemIn" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" id="submitBtn" class="bi bi-save" viewBox="0 0 16 16">
+            <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
+            </svg>
+        </li>
+    `)
+}
 
 function addNewItem() {
     let newItem = {
