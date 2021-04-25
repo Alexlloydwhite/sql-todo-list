0d7b2bc035bb042359acceb98b5e561aa1ec97e4
Alex White <alexlloydwhite@gmail.com>
2021-04-24 21:02:36 -0500

    refactored toggle complete to cfunciton correctly

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
     getList();
     // click listener to delete item from db
     $('.outPut').on('click', '#delete-item', deleteHandler);
-    // click listener to mark action item as complete TODO - Toggle!!
-    // $('.outPut').on('click', '.listItem', markAsComplete);
+    // click listener to toggle action item as complete
+    $('.outPut').on('click', '.toggleComplete', markAsComplete);
     //click listener to trigger from to add new task!
     $('#newTaskBtn').on('click', addNewTask);
     // click listener for submit new item 
@@ -93,7 +93,7 @@ function renderList(response) {
                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                 <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" id="${response[i].isComplete}" data-id="${response[i].id}" class="bi bi-file-check" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" id="${response[i].isComplete}" data-id="${response[i].id}" class="toggleComplete" viewBox="0 0 16 16">
                 <path d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
                 <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
                 </svg>                
