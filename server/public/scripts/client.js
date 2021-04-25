817cccbda33caf8ad1661613d85af86688252e11
Alex White <alexlloydwhite@gmail.com>
2021-04-24 21:50:57 -0500

    fixing missed indets

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -50,7 +50,7 @@ function addNewItem() {
         type: 'POST',
         url: '/todo',
         data: newItem
-    }).then(function (response) { 
+    }).then(function (response) {
         console.log('response from server:', response);
         // after POST, GET new data!
         getList();
@@ -142,7 +142,7 @@ function createEditInput() {
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" id="submitEdit" viewBox="0 0 16 16">
         <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
         </svg>
-    `) 
+    `)
     // get request to server to insert list item into input 
     $.ajax({
         type: 'GET',
