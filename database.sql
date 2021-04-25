2d40d60b110a08f89453a150e1d70cbfc30023e9
Alex White <alexlloydwhite@gmail.com>
2021-04-24 21:46:30 -0500

    updating DB

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@ CREATE TABLE "todo-list" (
 "list-item" varchar(120) not null,
 "is-complete" BOOLEAN DEFAULT FALSE);
 
-INSERT INTO "todo-list" ("list-item")
-VALUES ('do the dishes'), ('mow the lawn'), ('feed the cat'), ('laundry');
+INSERT INTO "todo-list" ("listItem")
+VALUES ('climb at bouldering gym'), ('graduate from prime'), ('feed the cat'), ('get a job');
 
 SELECT * FROM "todo-list";
