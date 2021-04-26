e64e583df05cc1dd5201b63ec282648f3d997c87
Alex White <alexlloydwhite@gmail.com>
2021-04-26 11:58:44 -0500

    updating database.sql file so that the app functions correctly

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "todo-list" (
 "id" SERIAL PRIMARY KEY,
-"list-item" varchar(120) not null,
-"is-complete" BOOLEAN DEFAULT FALSE);
+"listItem" varchar(120) not null,
+"isComplete" BOOLEAN DEFAULT FALSE);
 
 INSERT INTO "todo-list" ("listItem")
 VALUES ('climb at bouldering gym'), ('graduate from prime'), ('feed the cat'), ('get a job');
